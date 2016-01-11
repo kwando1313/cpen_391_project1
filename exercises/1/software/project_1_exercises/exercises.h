18b6435376c02884469544a996e6ed25b6b4eec7
angiecho <angela.hanna.cho@gmail.com>
2016-01-11 01:08:37 -0800

    Finished Exercise 1.1, added the multi-key functions

--- exercises/1/software/project_1_exercises/exercises.h
+++ exercises/1/software/project_1_exercises/exercises.h
@@ -19,36 +19,48 @@ int** createSquareMatrix(int size);
 void exercise_one() {
 
 	alt_up_character_lcd_dev * char_lcd_dev;
-		// open the Character LCD port
-		char_lcd_dev = alt_up_character_lcd_open_dev ("/dev/character_lcd_0");
-		if ( char_lcd_dev == NULL)
-			alt_printf ("Error: could not open character LCD device\n");
-		else
-			alt_printf ("Running Exercise 1.1\n");		/* Initialize the character display */
-		alt_up_character_lcd_init (char_lcd_dev);
-		/* Write "Reading Buttons" in the first row */
-		char first_row[] = "Reading Buttons\0";
-		alt_up_character_lcd_string(char_lcd_dev, first_row);
+	// open the Character LCD port
+	char_lcd_dev = alt_up_character_lcd_open_dev ("/dev/character_lcd_0");
+	if ( char_lcd_dev == NULL)
+		alt_printf ("Error: could not open character LCD device\n");
+	else
+		alt_printf ("Running Exercise 1.1\n");		/* Initialize the character display */
+	alt_up_character_lcd_init (char_lcd_dev);
+	/* Write "Reading Buttons" in the first row */
+	char first_row[] = "Reading KEYS:\0";
+	alt_up_character_lcd_string(char_lcd_dev, first_row);
 
-		int button_value;
-		while(1){
-			button_value = *(buttons);
-			alt_up_character_lcd_set_cursor_pos(char_lcd_dev, 0, 1);
+	int button_value;
 
-			/* Write to LCD, the button being pressed
-			 * 0x2 is key[1], 0x4 is key[2], 0x9 is key[3]
-			 */
-			if (button_value & 0x1)
-				alt_up_character_lcd_string(char_lcd_dev, "0");
-			else if (button_value & 0x2)
-				alt_up_character_lcd_string(char_lcd_dev, "1");
-			else if (button_value & 0x4)
-				alt_up_character_lcd_string(char_lcd_dev, "2");
-			else if (button_value & 0x8)
-				alt_up_character_lcd_string(char_lcd_dev, "3");
-			else
-				alt_up_character_lcd_string(char_lcd_dev, "X");
-		}
+	while(1){
+		button_value = *(buttons);
+		alt_up_character_lcd_set_cursor_pos(char_lcd_dev, 0, 1);
+		/* Write to LCD, the button being pressed
+		 * 0x2 is key[1], 0x4 is key[2], 0x9 is key[3]
+		 * If KEY[3] is pressed then position (0,1) says 3
+		 * If KEY[2] is pressed then position (1,1) says 2
+		 * ...
+		 * else the position is blank
+		 */
+
+		if (button_value & 0x8)
+			alt_up_character_lcd_string(char_lcd_dev, "3");
+		else alt_up_character_lcd_string(char_lcd_dev, " ");
+
+		if (button_value & 0x4)
+			alt_up_character_lcd_string(char_lcd_dev, "2");
+		else alt_up_character_lcd_string(char_lcd_dev, " ");
+
+		if (button_value & 0x2)
+			alt_up_character_lcd_string(char_lcd_dev, "1");
+		else alt_up_character_lcd_string(char_lcd_dev, " ");
+
+		if (button_value & 0x1)
+			alt_up_character_lcd_string(char_lcd_dev, "0");
+		else alt_up_character_lcd_string(char_lcd_dev, " ");
+
+		alt_up_character_lcd_cursor_off (char_lcd_dev);
+	}
 }
 
 /**
