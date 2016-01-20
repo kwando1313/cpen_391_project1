e3b3406b22e3dbb329a5ced24499c54b2ec69f46
William Tang <williamhientang@gmail.com>
2016-01-20 12:25:56 -0800

    1st commit to 1.4/1.6

--- exercises/1.3/software/project1/main.c
+++ exercises/1.3/software/project1/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "rs232.h"
+#include "touchscreen"
 
 int main(void) {
 
 	test_rs232();
+	//test_touch();
 
 	return 0;
 }
--- exercises/1.3/software/project1/touchscreen.h
+++ exercises/1.3/software/project1/touchscreen.h
@@ -1,11 +1,12 @@
 #ifndef TOUCHSCREEN_H_
 #define TOUCHSCREEN_H_
+#include <unistd.h>
 
 #define Touchscreen_Status 		(*(volatile unsigned char *)(0x84000230))
-#define Touchscreen_Control 	(*(volatile unsigned char *)(0x84000230))
-#define Touchscreen_TxData 		(*(volatile unsigned char *)(0x84000232))
-#define Touchscreen_RxData 		(*(volatile unsigned char *)(0x84000232))
-#define Touchscreen_Baud    	(*(volatile unsigned char *)(0x84000234))
+#define Touchscreen_Control 	((volatile unsigned char *)(0x84000230))
+#define Touchscreen_TxData 		(*(volatile unsigned char *)(0x84000233))
+#define Touchscreen_RxData 		((volatile unsigned char *)(0x84000233))
+#define Touchscreen_Baud    	(*(volatile unsigned char *)(0x84000238))
 
 
 /**************************************************************************
@@ -13,13 +14,13 @@
 **  Initialise touch screen controller
 *****************************************************************************/
 void Init_Touch(void){
-	// Program 6850 and baud rate generator to communicate with touchscreen
-	// send touchscreen controller an "enable touch" command
-
+	// Program 6850 and baud rate generator to communicate with touchscreen (Default baud rate)
 	Touchscreen_Baud = 0x05;
-	Touchscreen_Control = 0x55;
-	Touchscreen_Control = 0x01;
-	Touchscreen_Control = 0x12;
+
+	// send touchscreen controller an "enable touch" command
+	* Touchscreen_Control = 0x55;
+	*(Touchscreen_Control+1) = 0x01;
+	*(Touchscreen_Control+2) = 0x12;
 }
 /*****************************************************************************
 **   test if screen touched
@@ -27,7 +28,7 @@ void Init_Touch(void){
 int ScreenTouched( void){
 	// return TRUE if any data received from 6850 connected to touchscreen
 	// or FALSE otherwise
-	if (Touchscreen_RxData & 0x81) // some value received //0x81 = 10000001 = pen down command
+	if (*(Touchscreen_RxData) & 0x81) // some value received, 0x81 = 10000001 = pen down command
 		return 1; //TRUE
 	return 0; //FALSE
 }
@@ -44,13 +45,6 @@ void WaitForTouch(){
 void WaitForRelease(){
 	while(ScreenTouched());
 }
-
-/*****************************************************************************
-**   wait for screen to be released
-*****************************************************************************/
-void WaitForReadStatus(){
-	while(!(Touchscreen_Status & 0x00));
-}
 /* a data type to hold a point/coord */
 typedef struct{int x, y; } Point;
 /*****************************************************************************
@@ -61,20 +55,19 @@ Point GetPress(void){
 	// wait for a pen down command then return the X,Y coord of the point
 	// calibrated correctly so that it maps to a pixel on screen
 	WaitForTouch();
-	//Get value here for x
+	//Get value here for x, y
 	//Read first 2(?) bytes
-	WaitForReadStatus();
-	int x_first_half = Touchscreen_RxData;
-	WaitForReadStatus();
-	int x_second_half = Touchscreen_RxData;
-	WaitForReadStatus();
-	int y_first_half = Touchscreen_RxData;
-	WaitForReadStatus();
-	int y_second_half = Touchscreen_RxData;
+	while (!(Touchscreen_Status & 0x00));
+	int x_first_half = *(Touchscreen_RxData+1);
+	while (!(Touchscreen_Status & 0x00));
+	int x_second_half = *(Touchscreen_RxData+2);
+	while (!(Touchscreen_Status & 0x00));
+	int y_first_half = *(Touchscreen_RxData+3);
+	while (!(Touchscreen_Status & 0x00));
+	int y_second_half = *(Touchscreen_RxData+4);
 
-	int x = (x_second_half * 128) + (x_first_half);
-	int y = (y_second_half * 128) + (y_first_half);
-	p1 = Point(x,y);
+	p1.x = (x_second_half * 128) + (x_first_half);
+	p1.y = (y_second_half * 128) + (y_first_half);
 	return p1;
 }
 /*****************************************************************************
@@ -88,18 +81,31 @@ Point GetRelease(void){
 	WaitForRelease();
 
 	while (!(Touchscreen_Status & 0x00));
-	int x_first_half = Touchscreen_RxData;
+	int x_first_half = *(Touchscreen_RxData+1);
 	while (!(Touchscreen_Status & 0x00));
-	int x_second_half = Touchscreen_RxData;
+	int x_second_half = *(Touchscreen_RxData+2);
 	while (!(Touchscreen_Status & 0x00));
-	int y_first_half = Touchscreen_RxData;
+	int y_first_half = *(Touchscreen_RxData+3);
 	while (!(Touchscreen_Status & 0x00));
-	int y_second_half = Touchscreen_RxData;
+	int y_second_half = *(Touchscreen_RxData+4);
 
-	int x = (x_second_half * 128) + (x_first_half);
-	int y = (y_second_half * 128) + (y_first_half);
-	p1 = Point(x,y);
+	p1.x = (x_second_half * 128) + (x_first_half);
+	p1.y = (y_second_half * 128) + (y_first_half);
 	return p1;
+}
+
+void test_touch(void){
+	printf("Test touchscreen\n");
+	Init_Touch();
+
+	while(1) {
+		if(ScreenTouched()){
+			//printf("%c\n", *Touchscreen_RxData);
+			printf("touching\n");
+		}
+		// Point p1 = GetRelease();
+		// printf("Released Coordinate: (%i, %i)\n", p1.x, p1.y);
+	}
 }
 
 #endif /* TOUCHSCREEN_H_ */
