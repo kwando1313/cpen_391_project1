5d9fa68fb2981853b46f3eb8f6fcf412cd12c26e
Kwando <kwando9874123@hotmail.com>
2016-02-02 19:40:33 -0800

    Reverted to previous hardware, no more bleeding lines!

--- exercises/1.3/software/project1/graphics/menu.c
+++ exercises/1.3/software/project1/graphics/menu.c
@@ -1,15 +1,11 @@
-/*
- * menu.c
- *
- *  Created on: 2016-01-28
- *      Author: kwando1313
- */
-
 #include "misc_helpers.h"
 #include "graphics.h"
 #include "FontSize.h"
+#include "Directions.h"
 #include <stdio.h>
 #include <string.h>
+
+
 
 //Text box is left aligned and has text wrapping
 void draw_text_box(Point topLeft, int width, int height, int borderWidth, int borderColour, int fillColour, int textColour, char* text, int fontSize){
@@ -19,6 +15,9 @@ void draw_text_box(Point topLeft, int width, int height, int borderWidth, int bo
 	Point bottomRight = {topLeft.x + width, topLeft.y + height};
 
 	draw_filled_rectangle_border(topLeft, topRight, bottomLeft, bottomRight, fillColour, borderColour, borderWidth);
+	int initialX = 0;
+	int initialY = 0;
+
 
 	int fontSizePixelsWidth = 5;
 	int fontSizePixelsHeight = 7;
@@ -28,22 +27,49 @@ void draw_text_box(Point topLeft, int width, int height, int borderWidth, int bo
 		fontSizePixelsHeight = 14;
 	}
 
-	int initialX = topLeft.x + 10; //give 10 pixels margin
-	int initialY = topLeft.y + 10; //May make this an adjustable variable
-	for (int x = 0; x < textLength; x++){
+	initialX = topLeft.x + 10; //give 10 pixels margin
+	initialY = topLeft.y + 10; //May make this an adjustable variable
+
+	char * token = strtok(text, " ");
+
+	while (token != NULL){
+		int stringLength = (int) strlen(token);
+		if (initialX + (stringLength * (fontSizePixelsWidth + 1)) > topRight.x - 10){
+			initialX = topLeft.x + 10;
+			initialY = initialY + fontSizePixelsHeight + 1;
+		}
+		for (int x = 0; x < stringLength; x++){
+			if (token[x] == '\n'){
+				initialX = topLeft.x + 4;
+				initialY = initialY + fontSizePixelsHeight + 1;
+			}
+			if (fontSize == MEDIUM){
+				OutGraphicsCharFont2a(initialX, initialY, textColour, textColour, (int)token[x], 0);
+			}
+			else {
+				OutGraphicsCharFont1(initialX, initialY, textColour, textColour, (int)token[x], 0);
+			}
+			initialX = initialX + fontSizePixelsWidth + 1;
+		}
 		if (fontSize == MEDIUM){
-			OutGraphicsCharFont2a(initialX, initialY, textColour, textColour, (int)text[x], 0);
+			OutGraphicsCharFont2a(initialX, initialY, textColour, textColour, (int)" ", 0);
 		}
 		else {
-			OutGraphicsCharFont1(initialX, initialY, textColour, textColour, (int)text[x], 0);
+			OutGraphicsCharFont1(initialX, initialY, textColour, textColour, (int)" ", 0);
 		}
 		initialX = initialX + fontSizePixelsWidth + 1;
-
-		if (initialX > topRight.x + 10){
-			initialX = topLeft.x + 10;
-			initialY = initialY + fontSizePixelsHeight + 2;
-		}
+		token = strtok(NULL, " ");
 	}
+
+}
+
+//draw the info box on the RHS of the touchscreen
+void draw_information_box(char* text){
+	Point point8;
+
+	point8.x = 500;
+	point8.y = 0;
+	draw_text_box(point8, 300, 330,2, BLACK, WHITE, BLACK, text, SMALL);
 
 }
 
@@ -88,12 +114,165 @@ void draw_button(Point topLeft, int width, int height, int borderWidth, int bord
 	}
 }
 
+void draw_arrow(Point topLeft, int width, int height, int borderWidth, int borderColour, int fillColour, int direction){
+	Point topRight = {topLeft.x + width, topLeft.y};
+	Point bottomLeft = {topLeft.x, topLeft.y + height};
+	Point bottomRight = {topLeft.x + width, topLeft.y + height};
+	draw_filled_rectangle_border(topLeft, topRight, bottomLeft, bottomRight, fillColour, borderColour, borderWidth);
+
+	Point cornerOne = {0, 0};
+	Point cornerTwo = {0, 0};
+	Point cornerThree = {0, 0};
+
+	if (direction == UP){
+		cornerOne.x = topLeft.x + width/2;
+		cornerOne.y = topLeft.y + height/3;
+		cornerTwo.x = topLeft.x + width/3;
+		cornerTwo.y = topLeft.y + 2*height/3;
+		cornerThree.x = topLeft.x + 2*width/3;
+		cornerThree.y = topLeft.y + 2*height/3;
+	}
+	else if (direction == RIGHT){
+		cornerOne.x = topLeft.x + width/3;
+		cornerOne.y = topLeft.y + height/3;
+		cornerTwo.x = topLeft.x + width/3;
+		cornerTwo.y =  topLeft.y + 2*height/3;
+		cornerThree.x = topLeft.x + 2*width/3;
+		cornerThree.y = topLeft.y + height/2;
+	}
+	else if (direction == DOWN){
+
+		cornerOne.x = topLeft.x + width/3;
+		cornerOne.y = topLeft.y + height/3;
+		cornerTwo.x = topLeft.x + 2*width/3;
+		cornerTwo.y =  topLeft.y + height/3;
+		cornerThree.x = topLeft.x + width/2;
+		cornerThree.y = topLeft.y + 2*height/3;
+	}
+	else{
+
+		cornerOne.x = topLeft.x + width/3;
+		cornerOne.y = topLeft.y + height/2;
+		cornerTwo.x = topLeft.x + 2*width/3;
+		cornerTwo.y =  topLeft.y + height/3;
+		cornerThree.x = topLeft.x + 2*width/3;
+		cornerThree.y = topLeft.y + 2*height/3;
+	}
+
+	draw_triangle(cornerOne, cornerTwo, cornerThree, BLACK);
+}
+
+void draw_arrows(){
+	Point LPOINT = {10, 210};
+	Point RPOINT = {430, 210};
+	Point UPOINT = {220, 10};
+	Point DPOINT = {220, 410};
+
+	draw_arrow(LPOINT, 60, 60, 2, BLACK, WHITE, LEFT);
+
+	draw_arrow(RPOINT, 60, 60, 2, BLACK, WHITE, RIGHT);
+
+	draw_arrow(UPOINT, 60, 60, 2, BLACK, WHITE, UP);
+
+	draw_arrow(DPOINT, 60, 60, 2, BLACK, WHITE, DOWN);
+}
+
 //menu is buttons from top down
 void draw_menu(Point leftCorner, int width, int height, int borderWidth, int borderColour, int fillColour, int textColour, int fontSize, char** menuText){
 	int x = 0;
-	while (menuText[x] != NULL){
+	while (menuText[x] != ""){
 		draw_button(leftCorner, width, height, borderWidth, borderColour, fillColour, textColour, menuText[x], fontSize);
 		leftCorner.y+= height;
 		x++;
 	}
+}
+
+void init_screen(){
+	clear_screen(WHITE);
+
+		Point point6 = {500, 330};
+		Point point7 = {650, 330}; //Adjust these to fit within the margins...
+
+		char* firstTextArray[] = {"Info", "Photo", ""};
+		char* secondTextArray[] = {"Directions", "About", ""};
+
+		draw_information_box("Pathfinding Map (Team 22)\nAlex Charles\nAngela Cho\nCaleb Kwan\nWilliam Tang\n\nThis is our project!");
+		draw_menu(point6, 150, 75, 2, BLACK, WHITE, BLACK, SMALL, firstTextArray);
+
+		draw_menu(point7, 150, 75, 2, BLACK, WHITE, BLACK, SMALL, secondTextArray);
+
+		draw_arrows();
+}
+
+void about_screen(){
+//	clear_screen(WHITE);
+//
+//	Point point6 = {500, 330};
+//	Point point7 = {650, 330}; //Adjust these to fit within the margins...
+//
+//	char* firstTextArray[] = {"Info", "Photo", ""};
+//	char* secondTextArray[] = {"Directions", "About", ""};
+
+	draw_information_box("Pathfinding Map (Team 22)\nAngela Cho\nAndrew Charles\nCaleb Kwan\nWilliam Tang");
+//
+//	draw_menu(point6, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, firstTextArray);
+//
+//	draw_menu(point7, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, secondTextArray);
+//
+//	draw_arrows();
+}
+
+void info_screen(){
+	//clear_screen(WHITE);
+
+	//Point point6 = {500, 330};
+	//Point point7 = {650, 330}; //Adjust these to fit within the margins...
+
+	//char* firstTextArray[] = {"Info", "Photo", ""};
+	//char* secondTextArray[] = {"Directions", "About", ""};
+
+	draw_information_box("Need function to write info");
+
+	//draw_menu(point6, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, firstTextArray);
+
+	//draw_menu(point7, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, secondTextArray);
+
+	//draw_arrows();
+
+}
+
+void directions_screen(){
+//	clear_screen(WHITE);
+//
+//	Point point6 = {500, 330};
+//	Point point7 = {650, 330}; //Adjust these to fit within the margins...
+//
+//	char* firstTextArray[] = {"Info", "Photo", ""};
+//	char* secondTextArray[] = {"Directions", "About", ""};
+//
+	draw_information_box("Need function to draw directions");
+//
+//	draw_menu(point6, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, firstTextArray);
+//
+//	draw_menu(point7, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, secondTextArray);
+//
+//	draw_arrows();
+}
+
+void photo_screen(){
+//	clear_screen(WHITE);
+//
+//	Point point6 = {500, 330};
+//	Point point7 = {650, 330}; //Adjust these to fit within the margins...
+//
+//	char* firstTextArray[] = {"Info", "Photo", ""};
+//	char* secondTextArray[] = {"Directions", "About", ""};
+//
+	draw_information_box("need function to draw photo");
+//
+//	draw_menu(point6, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, firstTextArray);
+//
+//	draw_menu(point7, 150, 75, 1, BLACK, WHITE, BLACK, SMALL, secondTextArray);
+//
+//	draw_arrows();
 }
