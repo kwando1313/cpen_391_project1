67cbff30d7285bbdcfc6b30cc2310dfae2720ecf
Alex Charles <alexliam.charles@gmail.com>
2016-02-16 21:02:46 -0800

    added directions.h... didn't need to be removed before

---
+++ exercises/1.3/software/project1/graphics/Directions.h
@@ -0,0 +1,16 @@
+/*
+ * Directions.h
+ *
+ *  Created on: 2016-02-16
+ *      Author: Alex
+ */
+
+#ifndef DIRECTIONS_H_
+#define DIRECTIONS_H_
+
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
+
+#endif /* DIRECTIONS_H_ */
