84922cebed04354d772e670e20b04f3660cb7b1d
angiecho <angela.hanna.cho@gmail.com>
2016-02-25 01:49:13 -0800

    THIS WILL NOT COMPILE. Take a look at TIMER1.png and TIMER1(2).png first. Then compile Quartus, etc. Go to BSP editor and change sys_clk_timer to use timer_1 instead of timer_0

--- exercises/1.3/software/project1/main.c
+++ exercises/1.3/software/project1/main.c
@@ -6,14 +6,29 @@
 #include "load_node.h"
 #include "search.h"
 #include "gps.h"
+#include "system.h"
+#include "sys/alt_irq.h"
+#include <stdlib.h>
+#include "io.h"
+
+void interruptPrint(void){
+	printf ("interrupt enabled\n");
+	//Setting the Timeout back to 0 so that the timer
+	//can run again. Offset is 0
+	IOWR_16DIRECT(TIMER_1_BASE, 0, 0x0);
+}
 
 int main(void) {
-//	init_control();
-//	load_from_sd();
-//	s_listen();
-	//test_graphics();
-	clear_screen(WHITE);
-	Line(10, 10, 800, 80, BLACK);
+	//register and enable the IRQ line for our timer
+	//Obviously, change the register function to use read_gps instead of interruptPrint
+	//using interruptPrint since I don't have the gps right now
+	alt_irq_register(TIMER_1_IRQ, NULL, (void *)interruptPrint);
+	alt_irq_enable(TIMER_1_IRQ);
+	//set our timer to start, be continuous, and enable it's IRQ line. offset is 1
+	IOWR_16DIRECT(TIMER_1_BASE, 1, 0x7);
+
+	while(1);
+
 	printf("\nDONE\n");
 	return 0;
 }
